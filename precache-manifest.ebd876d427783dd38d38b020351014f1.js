c063e87296b09abfd834806161a9c0c26399512f
5377 <pradipamate07@gmail.com>
2020-05-17 21:27:04 +0530

    Updates

---
+++ precache-manifest.ebd876d427783dd38d38b020351014f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d85c40941e5e96c1c9e3bf5704f544bc",
+    "url": "/sales_product/index.html"
+  },
+  {
+    "revision": "ca6496863fc30663bcf0",
+    "url": "/sales_product/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c25ed640cc8c7dd75ebb",
+    "url": "/sales_product/static/js/2.4d181eac.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/sales_product/static/js/2.4d181eac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca6496863fc30663bcf0",
+    "url": "/sales_product/static/js/main.cf765c13.chunk.js"
+  },
+  {
+    "revision": "443201d38ab3e9963a06",
+    "url": "/sales_product/static/js/runtime-main.45b72bdd.js"
+  }
+]);
